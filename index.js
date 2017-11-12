64983b3e1196a9be719d5ab057593181c281b45e
Michael Delgadillo <protectordelafe@yahoo.com>
2017-11-12 01:11:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+const m1 = Math.floor(Math.random() * 10) + 1;
+//=> 8
+
+const m2 =Math.floor(Math.random() * 10) + 1;
+//=> 1
+
+const m3 =Math.floor(Math.random() * 10) + 1;
+//=> 6
+console.log(m1);
+console.log(m2);
+console.log(m3);
